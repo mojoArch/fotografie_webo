bdf72a49deb540120e6dbc16f8fc7949e1cef265
nesquikx014 <mojoArch@users.noreply.github.com>
2026-09-23 21:59:50 +0200

    eerste commit laravel aangemaakt

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Jamie Vis | Fotografie   </title>
+
+<body>
+    <header>
+        <h1>Jamie Vis</h1>
+       <nav>
+         <a href="/">Portfolio</a>
+       </nav>
+    </header>
+    
+    <main>
+        <h2>Fotografieportfolio</h2>
+        <p>Welkom bij mijn fotografieportfolio!</p>
+    </main>
+</body>
+
+
+    </head>
+</html>
